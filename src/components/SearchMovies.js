858ba5253c28bacdf1793b354e3ea687ca11355d
nsCoder123 <goranivankovic87@gmail.com>
2021-01-31 00:02:58 +0100

    sdsdfsdfsd

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar.js'
 import { Container,Row ,Col,Form,FormGroup,FormControl,Button } from 'react-bootstrap'
 import Axios from 'axios';
 import {Link}  from 'react-router-dom';
+import { FaWindowClose } from 'react-icons/fa';
 
 
 
@@ -10,76 +11,120 @@ export default class SearchMovies extends Component {
     constructor(props){
         super(props)
         this.state={
-            movie:''
+            movie:'',
+            poster:'',
+            title:'',
+            year:'',
+            runtime:'',
+            genre:'',
+            plot:'',
+            actors:'',
+            language:'',
+            spanovi:' | ',
+            textLanguage:'Language: ',
+            textStarting:'Starting:  '
+
+
         }
   
         this.eraseInputVal=this.eraseInputVal.bind(this)
         this.getMovies =this.getMovies.bind(this)
     }
+    componentDidMount(){
+      this.setState({
+          spanovi:'',
+          textLanguage:'',
+          textStarting:'',
+        
+      })
+
+    }
+
+   
+          
+           
+
+
+
     getMovies(e){
         this.setState({
             movie:e.target.value
         })
 
     }
-   
-   async eraseInputVal(e){
+
+    async eraseInputVal(e){
         e.preventDefault()
+      
     
       
         try {
             let inputBox = document.getElementById('inputBox')
-            let firstDiv = document.querySelector('.firstDiv')
-            let secondDiv =document.querySelector('.secondDiv')
-            
            
-          
+        
+       
+        
+            
+         
 
             const response = await Axios.get(`https://www.omdbapi.com/?t=${this.state.movie}&apikey=${process.env.REACT_APP_MOVIE_KEY}`);
-
-            firstDiv.innerHTML=`<img src="${response.data.Poster}".jpg>`;
-            secondDiv.innerHTML=`
-            <h2><span id="titleText">${response.data.Title}</span></h2><br></br>
-            <h5> ${response.data.Year} | ${response.data.Runtime} | ${response.data.Genre}</h5><br></br>
+   
+       
+    
          
-           
-            <p>${response.data.Plot} </p><br></br>
-            <p>Starting: ${response.data.Actors} </p><br></br>
-            <p>Language:${response.data.Language} </p>
-
-
-
-            
-            `
-            
-            
-            
 
            inputBox.value=''
            this.setState({
-               movie:''
+           
+            poster:response.data.Poster,
+            title:response.data.Title,
+            year:response.data.Year,
+            runtime:response.data.Runtime,
+            genre:response.data.Genre,
+            plot:response.data.Plot,
+            actors:response.data.Actors,
+            language:response.data.Language,
+            spanovi:' | ',
+            textLanguage:'Language: ',
+            textStarting:'Starting: '
+            
            })
+       
+        
+
+
           
           } catch (error) {
-            console.error(error);
-  
-            this.setState({
-                movie:''
-            })
-         
 
            
-          }
-     
+            let inputBox = document.getElementById('inputBox')
+        
+            inputBox.value=''
 
-         
+   this.setState({
+                spanovi:'',
+                textLanguage:'',
+                textStarting:'',
+              
+            })
+
+   
+          
       
 
-        
           
+
+              
+    
+           
+          }
+
+ 
            
     
     }
+    
+    
     
 
 
@@ -118,9 +163,19 @@ export default class SearchMovies extends Component {
                 <Container>
                     <Row>
                         <Col className="col-md-6 col-sm-12 col-12 firstDiv" style={h}>
+                        <img src={this.state.poster}/>
                         
                         </Col>
                         <Col className="col-md-6 col-sm-12 col-12 secondDiv" style={w}>
+                        <h2><span id="titleText">{this.state.title}</span></h2><br></br>
+
+                        <h5 > {this.state.year +' ' } <span>{this.state.spanovi}</span> { this.state.runtime} <span>{this.state.spanovi}</span> {this.state.genre}</h5><br></br>
+         
+           
+         <p>{this.state.plot} </p><br></br>
+         <p><span>{this.state.textStarting} </span> {this.state.actors} </p><br></br>
+         <p><span>{this.state.textLanguage} </span>  {this.state.language} </p>
+         
                        
                         
                         </Col>
